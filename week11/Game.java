ea9a7d33b47b636f5bd0231bafa7bed64f81f40a
name7951 <65230125+name7951@users.noreply.github.com>
2023-05-23 15:37:41 +0900

    Update Game.java

--- week11/Game.java
+++ week11/Game.java
@@ -16,6 +16,7 @@ public class Game {
 		}
 
 		printArray(a);
+		System.out.println();
 
 		System.out.println("아무 숫자를 입력하면 숫자를 감춥니다.");
 		int num = input.nextInt();
@@ -26,16 +27,21 @@ public class Game {
 		System.out.println("어던 숫자들이 있었는지 맞춰보세요");
 		System.out.println("기회는 5번 입니다.");
 
+		int correct = 0; // 맞춘 숫자 개수를 저장하는 변수
+
 		for (int i = 0; i < 5; i++) {
 			int key = input.nextInt();
 
-			
-
+			if (search(a, a.length, key) != -1) {
+				correct++;
+			}
 		}
+
+		System.out.println("5문제 중 총 " + correct + "개 맞췄습니다!");
 
 	}
 
-	public static void printArray(int[] array) {
+	public static void printArray(int[] array) { // 배열을 출력하는 메소드
 
 		System.out.println("기억력 테스트 게임");
 		for (int i = 0; i < array.length; i++)
@@ -44,22 +50,22 @@ public class Game {
 
 	}
 
-	public static int search(int[] x, int n, int key) {
+	public static int search(int[] x, int n, int key) { // 이진 탐색을 수행해서 인덱스를 변환하는 메소드
 
 		int lo = 0;
 		int hi = n - 1;
 		int mid;
 
 		while (hi >= lo) {
-			mid = (hi + lo) / 2;
-			if (key == x[mid]) // 키를 발견
-				return mid; // 인덱스를 반환
-			if (key < x[mid])
-				hi = mid - 1;
-			else
-				lo = mid + 1;
-		}
-		return -1; // 키를 찾지 못함
+            mid = (hi + lo) / 2;
+            if (key == x[mid])
+                return mid; // 키를 발견하면 인덱스를 반환
+            if (key < x[mid])
+                hi = mid - 1;
+            else
+                lo = mid + 1;
+        }
+        return -1; // 키를 찾지 못한 경우 -1을 반환
 
 	}
 
