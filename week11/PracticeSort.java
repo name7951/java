89798dd9efb3bee1bcc678a1dfaf50f277481102
name7951 <65230125+name7951@users.noreply.github.com>
2023-05-23 15:37:58 +0900

    Update PracticeSort.java

--- week11/PracticeSort.java
+++ week11/PracticeSort.java
@@ -24,7 +24,7 @@ public class PracticeSort {
 
 	}
 
-	public static void printArray(int[] array) {
+	public static void printArray(int[] array) { // 배열을 출력하는 메소드
 
 		System.out.println("처음 배열");
 		for (int i = 0; i < array.length; i++)
@@ -33,24 +33,24 @@ public class PracticeSort {
 
 	}
 
-	public static void sort(int[] x, int n) {
+	public static void sort(int[] x, int n) { // 삽입 정렬을 수행하는 메소드
 
 		for (int i = 1; i < n; i++) {
-			insert(x, i);
+			insert(x, i); // 배열의 i번째 요소를 삽입 정렬
 		}
 
 	}
 
-	public static void insert(int[] x, int i) {
+	public static void insert(int[] x, int i) { // 삽입 정렬을 수행하는 메소드
 
-		int temp = x[i];
+		int temp = x[i]; // 삽입할 값을 변수에 저장
 
 		int j = i - 1;
 		while (j >= 0 && temp < x[j]) {
-			x[j + 1] = x[j];
+			x[j + 1] = x[j]; // 오른쪽으로 한 칸 이동
 			j--;
 		}
-		x[j + 1] = temp;
+		x[j + 1] = temp; // 삽입할 값을 맞는 위치에 삽입
 
 	}
 
